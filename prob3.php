22940782c39bca101491182b58b0c1b36e77518b
rtsubaki00 <rinaaaaaaaaaa0811@gmail.com>
2020-03-25 00:11:17 +0900

    Create  prob3.php

---
+++ prob3.php
@@ -0,0 +1,14 @@
+<?php 
+$a = 3;
+switch($a) {
+    case 1:
+      echo 'おはよう';
+    case 2:
+      echo 'こんにちは';
+    case 3:
+      echo 'こんばんは';
+      break;
+    default:
+      echo 'さようなら';
+      break;
+}
