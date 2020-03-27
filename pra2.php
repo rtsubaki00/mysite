08be534e25aaa4cb842982d42762a02d34d9611d
rtsubaki00 <rinaaaaaaaaaa0811@gmail.com>
2020-03-27 22:26:17 +0900

    Create prob2.php

---
+++ pra2.php
@@ -0,0 +1,19 @@
+<table border="1">
+<?php
+   for($a = 1; $a <= 9; $a++){
+       echo '<tr>';
+       for($i = 1; $i <= 9; $i++){
+           $ans = $a * $i;
+           if($i % 2 === 0){
+               echo '<td>'.$i.'x'.$a.'='.$ans.'</td>';
+           } else {
+               echo '<td bgcolor="#C0C0C0">'.$i.'x'.$a.'='.$ans.'</td>';
+           } 
+           
+
+         
+       }
+       echo '</tr>';
+   }
+?>
+</table>
