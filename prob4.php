bda37f62759816fa27826d6772abcba099eb0c55
rtsubaki00 <rinaaaaaaaaaa0811@gmail.com>
2020-03-27 14:22:27 +0900

    Fix

--- prob4.php
+++ prob4.php
@@ -1,20 +1,16 @@
 <?php
 $a = 3;
-switch($a){
-  case $a >= 3;
-   print 'こんにちは';
-break;
-  case 1;
-  print '今日は良い天気だね';
-break;
-  case 2;
-  print '今日は寒いね';
-break;
-  case 3;
-  print '今日は湿度が高いね';
-break;
-  default;
-  print 'さようなら';
-break;
-}
+if($a >= 3){
+  echo 'こんにちは';
+  if ($a == 1){
+    echo '今日は良い天気だね';
+  } elseif ($a == 2){
+    echo '今日は寒いね';
+  } elseif ($a == 3){
+    echo '今日は湿度が高いね';
+  } else {
+    echo 'さようなら';
+  }
+} 
+
 ?>
