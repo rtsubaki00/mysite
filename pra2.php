cf3e5829acba39cd30736879af644dfd47218c49
rtsubaki00 <rinaaaaaaaaaa0811@gmail.com>
2020-04-02 11:06:51 +0900

    Create pra2.php

--- pra2.php
+++ pra2.php
@@ -3,14 +3,25 @@
    for($a = 1; $a <= 9; $a++){
        echo '<tr>';
        for($i = 1; $i <= 9; $i++){
-           $ans = $a * $i;
-           if($i % 2 === 0){
-               echo '<td>'.$i.'x'.$a.'='.$ans.'</td>';
-           } else {
-               echo '<td bgcolor="#C0C0C0">'.$i.'x'.$a.'='.$ans.'</td>';
-           } 
-       }
-       echo '</tr>';
-   }
+        $ans = $a * $i;
+
+        $f1 = '';
+        $f2 = '';
+        if($ans % 2 === 0){
+            $bg = '';
+        } else {
+            $bg = 'bgcolor="#C0C0C0"';
+        }
+
+
+            if($ans == 18){
+                $f1 = '<font color="#ff0000">';
+                $f2 = '</font>';
+             }  
+     echo "<td $bg>" . $f1 . $i . 'x' . $a . '=' . $ans . $f2 . '</td>';
+ }
+        echo '</tr>';
+     }  
 ?>
 </table>
+
