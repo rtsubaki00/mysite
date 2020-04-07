2fe0f8d4a5959460737fb20fd69160661a0a10c3
rtsubaki00 <rinaaaaaaaaaa0811@gmail.com>
2020-04-07 19:21:16 +0900

    Fix prob4.php

--- prob4.php
+++ prob4.php
@@ -1,21 +1,28 @@
 <?php
 $a = 3;
-if($a >= 3){
-  echo 'こんにちは';
-  echo '<br>';
-  if ($a == 1){
-    echo '今日は良い天気だね';
-    echo '<br>';
-  } elseif ($a == 2){
-    echo '今日は寒いね';
-    echo '<br>';
-  } elseif ($a == 3){
-    echo '今日は湿度が高いね';
-    echo '<br>';
-  } 
-} else {
-  echo 'さようなら';
-  echo '<br>';
-}
+   if($a == 1) {
+       echo '今日は良い天気だね';
+       echo '<br>';
+   } elseif($a == 2){
+       echo '今日は寒いね';
+       echo '<br>';
+   } elseif($a == 3){
+       echo '今日は湿度が高いね';
+       echo '<br>';
+   }
+
+   if($a >= 3){
+       echo 'こんにちは';
+       echo '<br>';
+   }
+   
+   if($a <= 0){
+       echo 'さようなら';
+       echo '<br>';
+   }
+
+   
+
+
 
 ?>
