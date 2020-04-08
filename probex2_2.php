2feb3de6cc1e130fb6bb8eb6a5c645ae4cab9af4
rtsubaki00 <rinaaaaaaaaaa0811@gmail.com>
2020-04-08 22:38:07 +0900

    Fix probex2_2.php

--- probex2_2.php
+++ probex2_2.php
@@ -1,3 +1,30 @@
 <?php
-readfile("test.php");
+ 
+$filepath = 'testdata.csv';
+ 
+
+$file = new SplFileObject($filepath);
+ 
+
+$file->setFlags(SplFileObject::READ_CSV);
+ 
+
+ 
+
+foreach ($file as $line) {
+ 
+ 
+  
+ 
+  
+  $cnt = count($line);
+ 
+  
+ 
+  for($i = 0; $i < $cnt; $i++){
+    echo $line[$i].'<br>';
+  }
+}
+echo 'Thank you';
+ 
 ?>
