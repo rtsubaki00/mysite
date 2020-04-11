47d5939273067119f01b0528839bf25f78e8a89f
rtsubaki00 <rinaaaaaaaaaa0811@gmail.com>
2020-04-11 22:54:12 +0900

    Delete foreach csv probex2_2.php

--- probex2_2.php
+++ probex2_2.php
@@ -1,32 +1,5 @@
 <?php
 
- 
-$filepath = 'testdata.csv';
- 
-
-$file = new SplFileObject($filepath);
- 
-
-$file->setFlags(SplFileObject::READ_CSV);
- 
-
- 
-
-foreach ($file as $line) {
- 
- 
-  
- 
-  
-  $cnt = count($line);
- 
-  
- 
-  for($i = 0; $i < $cnt; $i++){
-    echo $line[$i].'<br>';
-  }
-}
-
 $a = fopen("testdata.csv", "a");
 @fwrite($a, "Thank you");
 fclose($a);
