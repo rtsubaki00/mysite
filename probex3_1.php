e68b5fb3c2b73694bc6566c1797760723d40c0b4
rtsubaki00 <rinaaaaaaaaaa0811@gmail.com>
2020-04-14 15:12:15 +0900

    Create Probex3_1.php Probex3_2.php <class><static>

---
+++ probex3_1.php
@@ -0,0 +1,14 @@
+<?php
+class Month {
+    public $month;
+
+    function showDate(){
+        echo "５か月後の月は".date("m", strtotime($this -> month. "+5 month"))."月です。";
+    }
+}
+
+$date = new Month;
+$date -> month = "2020-04-14";
+
+$date -> showDate();
+?>
---
+++ probex3_2.php
@@ -0,0 +1,8 @@
+<?php
+class Month {
+    public static $val = "2020-04-14";
+}
+
+$value = Month::$val;
+echo "５か月後の月は".date("m", strtotime($value. "+5 month"))."月です。";
+?>
