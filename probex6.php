598cffd22772a929041defbd4fca1c97eea35fe3
rtsubaki00 <rinaaaaaaaaaa0811@gmail.com>
2020-05-09 11:18:28 +0900

    Create probex6_1.php

---
+++ probex6.php
@@ -0,0 +1,12 @@
+<?php
+$db = new mysqli('localhost', 'hellouser', 'rpass', 'hellohhelo');
+if ($db->connect_error) {
+  echo $db->connect_error;
+ exit();
+} else {
+   $db->set_charset("utf8");
+}
+echo "<table>";
+
+
+?>
---
+++ probex6_1.php
@@ -0,0 +1,14 @@
+<?php
+$sql = "SELECT id, text, number FROM hello_table";
+if ($result = $db->query($sql)) {
+while ($row = $result->fetch_assoc()){
+
+   
+    
+echo "<tr><td>" . $row["id"] . "</td><td>" . $row["text"] . "</td><td>". $row["number"] . "</td></tr>";
+}
+$result->close();
+}
+echo "</table>";
+$db->close();
+?>
